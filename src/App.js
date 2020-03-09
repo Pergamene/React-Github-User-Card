37b6a780d316b3fb70aef5a00d67aa94cad608e0
Pergamene <smartz373@gmail.com>
2020-03-09 17:46:32 -0600

    got headed up

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+
+import Header from './components/Header.jsx';
+
+const useStyles = makeStyles({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    maxWidth: '800px',
+    margin: '50px auto',
+    padding: '30px',
+  },
+});
 
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      
+    <div className={classes.root}>
+      <Header />
     </div>
   );
 }
